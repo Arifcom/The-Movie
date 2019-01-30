2845d52e4c0950e7c4d0452d036203ec23f6b9df
Arif Budiman <arif.56.budiman@gmail.com>
2019-01-30 20:04:25 +0700

    Commit

--- appfavorite/src/main/java/directory/themoviefavorite/MainActivity.java
+++ appfavorite/src/main/java/directory/themoviefavorite/MainActivity.java
@@ -1,14 +1,58 @@
 package directory.themoviefavorite;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import directory.themoviefavorite.db.DatabaseContract;
+import directory.themoviefavorite.modules.adapter.FavoriteAdapter;
+import static directory.themoviefavorite.db.DatabaseContract.FavoriteColumns.CONTENT_URI;
 
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements
+        LoaderManager.LoaderCallbacks<Cursor> {
+    private FavoriteAdapter favoriteAdapter;
+    ListView lvFavorite;
+    private final int LOAD_FAVORITE_ID = 110;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.favorite_card);
+        getSupportActionBar().setTitle("The Movie Favorite List");
+        lvFavorite = (ListView) findViewById(R.id.lv_favorite);
+        favoriteAdapter = new FavoriteAdapter(this, null, true);
+        lvFavorite.setAdapter(favoriteAdapter);
+        getSupportLoaderManager().initLoader(LOAD_FAVORITE_ID, null, this);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getSupportLoaderManager().restartLoader(LOAD_FAVORITE_ID, null, this);
+    }
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return new CursorLoader(this, CONTENT_URI, null, null, null, null);
+    }
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        favoriteAdapter.swapCursor(data);
+    }
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        favoriteAdapter.swapCursor(null);
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
---
+++ appfavorite/src/main/java/directory/themoviefavorite/modules/adapter/FavoriteAdapter.java
@@ -0,0 +1,40 @@
+package directory.themoviefavorite.modules.adapter;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import directory.themoviefavorite.R;
+
+import static directory.themoviefavorite.db.DatabaseContract.FavoriteColumns.ORIGINAL_TITLE;
+import static directory.themoviefavorite.db.DatabaseContract.FavoriteColumns.POSTER_PATH;
+import static directory.themoviefavorite.db.DatabaseContract.FavoriteColumns.OVERVIEW;
+import static directory.themoviefavorite.db.DatabaseContract.FavoriteColumns.RELEASE_DATE;
+import static directory.themoviefavorite.db.DatabaseContract.getColumnString;
+
+public class FavoriteAdapter extends CursorAdapter {
+    public FavoriteAdapter(Context context, Cursor c, boolean autoRequery) {
+        super(context, c, autoRequery);
+    }
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
+        return LayoutInflater.from(context).inflate(R.layout.favorite_item, viewGroup, false);
+    }
+    @Override
+    public Cursor getCursor() {
+        return super.getCursor();
+    }
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        if (cursor != null){
+            TextView original_title = (TextView)view.findViewById(R.id.original_title);
+            TextView release_date = (TextView)view.findViewById(R.id.release_date);
+            original_title.setText(getColumnString(cursor,ORIGINAL_TITLE));
+            release_date.setText(getColumnString(cursor,RELEASE_DATE));
+        }
+    }
+}
